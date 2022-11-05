23a4f4f45d7c4ab433fb00b94eec27478d0752b0
lilyshowlader <lilyshowlader@gmail.com>
2022-11-05 13:30:41 -0400

    add redirect link

--- main_app/models.py
+++ main_app/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
 
@@ -11,3 +12,7 @@ class Finch(models.Model):
 
   def __str__(self):
     return self.name
+  
+  # Add this class method
+  def get_absolute_url(self):
+    return reverse('finches_detail', kwargs={'finch_id': self.id})
